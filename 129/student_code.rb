f1d331560a3f2b6cbcb2fe4525fae1656ea46537
Seth Geyer <seth.geyer@gmail.com>
2013-04-22 14:56:48 -0600

    add stars

--- 129/student_code.rb
+++ 129/student_code.rb
@@ -1,1 +1,13 @@
-puts "change me"
+#Ask for a number, then print that number of stars. (i.e. if 
+#the user enters 3, print ***).
+
+puts "Enter a number:"
+number = readline.to_i
+star = "*"
+
+while number > 1 
+  star = "#{star}*"
+  number = number -1
+end
+
+puts star
