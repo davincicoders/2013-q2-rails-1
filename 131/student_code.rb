6e11e0dbebf0a5325e97b8ef7356ee69df897df5
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-04-22 17:28:15 -0600

    Pete & Repeat

--- 131/student_code.rb
+++ 131/student_code.rb
@@ -1,1 +1,8 @@
-puts "change me"
+# Wait for a line of user input, then print it back. (Do not have a puts line asking for the user to enter text). Repeat. Stop when the user presses Enter by itself (the input is empty).
+
+strInput = readline.chomp
+
+while strInput != ""
+  puts strInput
+  strInput = readline.chomp
+end
