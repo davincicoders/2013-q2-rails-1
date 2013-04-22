38b357475de96ea5c67a84fb6d452d556bbdb22e
John Walker <johnwalker@siu.edu>
2013-04-22 14:08:53 -0600

    GitHub Challenge 132 - keep a running total of user input. puts when blank line is entered.

--- 132/student_code.rb
+++ 132/student_code.rb
@@ -1,1 +1,10 @@
-puts "change me"
+n = 0
+text = readline.chomp
+if text != ""
+  while text != ""
+  n = (n + text.to_i) # Convert text to int and add to running total.
+  text = readline.chomp
+  end
+else
+end
+puts n
