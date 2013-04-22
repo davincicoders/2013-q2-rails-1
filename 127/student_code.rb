61bca403c1a17e1ed372581d8d42bccd3c4478e9
Seth Geyer <seth.geyer@gmail.com>
2013-04-22 14:34:27 -0600

    number+1

--- 127/student_code.rb
+++ 127/student_code.rb
@@ -1,1 +1,7 @@
-puts "change me"
+#Ask for a number, and print out a number one higher. 
+#(i.e. if the user enters 3, print 4). (Hint: use 
+#readline.chomp.to_i instead of readline.chomp.)
+
+puts "Enter a number:"
+user_input = readline.chomp.to_i
+puts user_input + 1
