ea13d014cc6182fdfc2c4b17ebbeeb47ace66d2b
Julian Hentea <redpenpress@gmail.com>
2013-04-22 15:39:34 -0700

    128 passed tests

--- 128/student_code.rb
+++ 128/student_code.rb
@@ -1,1 +1,22 @@
-puts "change me"
+puts "Enter the password:"
+
+password = readline.chomp
+if password == "DVC"
+   puts "Correct"
+elsif password == "DVc"
+  puts "Correct"
+elsif password == "DvC"
+  puts "Correct"
+elsif password == "Dvc"
+  puts "Correct"
+elsif password == "dVC"
+  puts "Correct"
+ elsif password == "dVc"
+  puts "Correct"
+elsif password == "dvC"
+  puts "Correct"
+elsif password == "dvc"
+  puts "Correct" 
+ else
+  puts "Incorrect"
+end
