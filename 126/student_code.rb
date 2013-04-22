834fb0e5bef92b9477128efbbdee5a9808d4e487
Seth Geyer <seth.geyer@gmail.com>
2013-04-22 14:22:15 -0600

    password request

--- 126/student_code.rb
+++ 126/student_code.rb
@@ -1,1 +1,11 @@
-puts "change me"
+# Request a password from the user.  If the password is correct, print "Correct", otherwise print "Incorrect".
+
+puts "Enter the password:"
+password = readline.chomp
+
+if password == "dvc"
+puts "Correct"
+else
+  puts "Incorrect"
+end
+
