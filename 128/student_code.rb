09517ddb2798886a50483c9006d3c04a0bfd34f6
Seth Geyer <seth.geyer@gmail.com>
2013-04-22 14:43:19 -0600

    password matching

--- 128/student_code.rb
+++ 128/student_code.rb
@@ -1,1 +1,15 @@
-puts "change me"
+#Ask for a password, then print Correct if 
+#the password is dvc or DVC or Dvc or any 
+#other uppercase-lowercase possibility. 
+#Otherwise print Incorrect.
+
+puts "Enter the password:"
+user_input = readline.chomp.downcase
+password = "dvc"
+
+
+if user_input == password
+  puts "Correct"
+else
+  puts "Incorrect"
+end
