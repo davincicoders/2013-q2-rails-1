1022b88dffbcd2e24625f47e2c2968d6c60dc1a7
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-04-22 17:16:29 -0600

    Password fun...

--- 128/student_code.rb
+++ 128/student_code.rb
@@ -1,1 +1,12 @@
-puts "change me"
+# Ask for a password, then print Correct if the password is dvc or DVC or Dvc or any other uppercase-lowercase possibility. Otherwise print Incorrect.
+strPass = ""
+
+puts "Enter the password:"
+
+strPass = readline.chomp
+
+if strPass.upcase == "DVC"
+  puts "Correct"
+else
+  puts "Incorrect"
+end
