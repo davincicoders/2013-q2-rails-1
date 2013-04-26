af98d70daa015f0ad42e006146b24830f92f4eaf
Julian Hentea <redpenpress@gmail.com>
2013-04-26 11:20:34 -0700

    fixed bugs

--- 163/163.rb
+++ 163/163.rb
@@ -1,11 +1,12 @@
 # The intention of this program is to reverse a list of words,
 # but there are bugs.  Find and fix the bug to make the tests pass.
 
+
 puts "Enter a list of words, separated by spaces (e.g. big green frog):"
 input = readline.chomp
 words = input.split(" ")
-reversed_words = words
-for i in 0..(words.size)
-  reversed_words[i] = reversed_words[words.size - i]
+reversed_words = []
+for i in 0..(words.size - 1)
+  reversed_words[i] = words[(words.size - 1) - i]
 end
 puts reversed_words.join(" ")
