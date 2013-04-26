d9e21c397587ac37d3e7f1248b112b538400864a
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-04-26 12:22:36 -0600

    splitting cities..

--- 177/177.rb
+++ 177/177.rb
@@ -1,5 +1,13 @@
-file = File.open("input.txt", "r")
-for line in file.each_line
-  puts line
+require "pry"
+
+#binding.pry
+
+list = []
+
+file = File.read("colorado-cities.csv")
+list = file.to_s.split("\n")
+
+for i in 1..(list.size - 1)
+  list[i] = list[i].split(",")
+  puts list[i][0] if list[i][3].to_i <= 500000 
 end
-file.close
