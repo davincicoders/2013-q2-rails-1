147b9f55b0d30c01e095b7ed491ef0f2dcc764b0
Daniel Stutzman <dtstutz@gmail.com>
2013-04-26 11:15:54 -0600

    added pry, attempted bug fix by subtracting 1

--- 163/163.rb
+++ 163/163.rb
@@ -1,11 +1,15 @@
+require "pry"
+
 # The intention of this program is to reverse a list of words,
 # but there are bugs.  Find and fix the bug to make the tests pass.
+
+#binding.pry
 
 puts "Enter a list of words, separated by spaces (e.g. big green frog):"
 input = readline.chomp
 words = input.split(" ")
 reversed_words = words
 for i in 0..(words.size)
-  reversed_words[i] = reversed_words[words.size - i]
+  reversed_words[i] = reversed_words[words.size - i - 1]
 end
 puts reversed_words.join(" ")
