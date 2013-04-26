9275e922ef9cab815cef632fc1c6e22aa2395031
Leor Manelis <leormanelis@gmail.com>
2013-04-26 11:21:22 -0600

    reversed the words

--- 163/163.rb
+++ 163/163.rb
@@ -1,11 +1,14 @@
+require "pry"
+
 # The intention of this program is to reverse a list of words,
 # but there are bugs.  Find and fix the bug to make the tests pass.
+
+#binding.pry
 
 puts "Enter a list of words, separated by spaces (e.g. big green frog):"
 input = readline.chomp
 words = input.split(" ")
-reversed_words = words
-for i in 0..(words.size)
-  reversed_words[i] = reversed_words[words.size - i]
-end
-puts reversed_words.join(" ")
+words = words.reverse
+puts words.join(" ")
+
+
