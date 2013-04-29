9aeb170add50fe70c4ac81dc41d54deb298ef04c
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-04-29 12:04:04 -0600

    Edited, check for correct username/password

--- 189/189.rb
+++ 189/189.rb
@@ -6,6 +6,14 @@ puts "Enter password:"
 entered_password = readline.chomp
 
 # TODO: print out one of the following messages:
-#   Correct password.
-#   Incorrect password.
-#   Unknown username.
+objUser = G189User.where(username: entered_username).first
+
+#puts objUser.username
+
+if objUser && objUser.password == entered_password
+  puts "Correct password."
+elsif objUser && objUser.password != entered_password
+  puts "Incorrect password."
+else
+  puts "Unknown username."
+end
