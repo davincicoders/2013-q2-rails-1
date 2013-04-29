80ffb54862fea7c406353bf8cb5af6c928670a16
Seth Geyer <seth.geyer@gmail.com>
2013-04-29 14:09:10 -0600

    Upload File

--- 189/189.rb
+++ 189/189.rb
@@ -1,3 +1,27 @@
+# Seth
+=begin
+require './connect_to_db'
+
+puts "Enter username:"
+entered_username = readline.chomp
+query = G189User.where(username: "#{entered_username}").first
+user = query.username
+
+  if query == nil
+    puts "Unknown username."
+  else
+      correct_password = query.password
+    puts "Enter password:"
+    entered_password = readline.chomp
+    if entered_password == correct_password
+      puts "Correct password."
+    else
+      puts "Incorrect password"
+    end
+  end
+=end
+
+
 require './connect_to_db'
 
 puts "Enter username:"
@@ -5,7 +29,17 @@ entered_username = readline.chomp
 puts "Enter password:"
 entered_password = readline.chomp
 
-# TODO: print out one of the following messages:
-#   Correct password.
-#   Incorrect password.
-#   Unknown username.
+query = G189User.where(username: "#{entered_username}").first
+
+  if query == nil
+    puts "Unknown username."
+  elsif 
+    user = query.username
+    correct_password = query.password
+
+    if entered_password == correct_password
+      puts "Correct password."
+    else
+      puts "Incorrect password."
+    end
+  end
