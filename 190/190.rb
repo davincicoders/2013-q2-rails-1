681d01a4bb8ee7fa0481a68bec1687eb984f53e5
John Walker <johnwalker@siu.edu>
2013-04-29 16:53:24 -0600

    Brain fried. Posting GitHub Challenge 190. argh.

--- 190/190.rb
+++ 190/190.rb
@@ -2,3 +2,24 @@ require './connect_to_db'
 
 puts "Enter a year:"
 entered_year = readline.chomp.to_i
+
+# took_office
+# left_office
+# president
+
+
+
+pres_all = President.order("id")
+
+for pres in pres_all
+
+entered = pres.took_office.split("/")
+enteredi = entered[2].to_i
+
+ended = pres.left_office.split("/")
+exiti = ended[2].to_i
+
+  if enteredi <= entered_year && entered_year <= exiti
+    puts pres.president
+  end
+end  
