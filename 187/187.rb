80cf324f8407b80927e38b8e604d037a7a68842e
Seth Geyer <seth.geyer@gmail.com>
2013-04-29 18:03:44 -0600

    pow

--- 187/187.rb
+++ 187/187.rb
@@ -8,3 +8,13 @@ puts "Word to search for in station name, or leave blank:"
 word = readline.chomp
 
 # TODO: Implement printing out the station names that match the filters
+
+prop = BCycleStation.where("property_type like ?", "%#{property_type}%")
+pow = prop.where("power_type like ?", "%#{power_type}%")
+stations = pow.where("station_name like ?", "%#{word}%")
+
+
+
+for station in stations
+puts station.station_name
+end
