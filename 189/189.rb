c144978506a5dd5d0d264da488d95856ee1ffba0
Leor Manelis <leormanelis@gmail.com>
2013-04-30 12:41:47 -0600

    Copied Daniels code as i am trying to understand psql

--- 189/189.rb
+++ 189/189.rb
@@ -5,6 +5,18 @@ entered_username = readline.chomp
 puts "Enter password:"
 entered_password = readline.chomp
 
+user = G189User.where(username: entered_username).first
+
+if user != nil
+  if user.password == entered_password
+    puts "Correct password."
+  else
+    puts "Incorrect password."
+  end
+else
+  puts "Unknown username."
+end
+
 # TODO: print out one of the following messages:
 #   Correct password.
 #   Incorrect password.
