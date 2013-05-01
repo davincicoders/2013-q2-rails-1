c0738ec00416b84a5a27636967724a4f9944e0fc
Student <student@student-3.local>
2013-05-01 10:12:15 -0600

    189

--- 189/189.rb
+++ 189/189.rb
@@ -1,9 +1,20 @@
 require './connect_to_db'
+require "pry"
 
 puts "Enter username:"
 entered_username = readline.chomp
 puts "Enter password:"
 entered_password = readline.chomp
+
+binding.pry
+
+objUser = G189User.where(username: entered_username).first
+
+puts case 
+    when objUser && objUser.password == entered_password; "Correct password."
+    else "Incorrect password."
+    end 
+
 
 # TODO: print out one of the following messages:
 #   Correct password.
