937cc6f300db94628ad0a52f6ef42b065e79a165
Julian Hentea <redpenpress@gmail.com>
2013-05-01 06:52:06 -0700

    passed tests

--- 187/187.rb
+++ 187/187.rb
@@ -8,3 +8,17 @@ puts "Word to search for in station name, or leave blank:"
 word = readline.chomp
 
 # TODO: Implement printing out the station names that match the filters
+
+stations = BCycleStation.order('id')
+
+if property_type != ""
+  stations = stations.where(property_type: property_type)
+elsif power_type != ""
+  stations = stations.where(power_type: power_type)
+elsif word != ""
+  stations = stations.where("station_name like ?", "%#{word}%")
+end
+
+for station in stations
+  puts station.station_name
+end
