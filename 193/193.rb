a3fd6cf0824168b6d81e5519039d79b3f6003a79
Seth Geyer <seth.geyer@gmail.com>
2013-05-01 16:26:04 -0600

    actor name

--- 193/193.rb
+++ 193/193.rb
@@ -1,8 +1,23 @@
+=begin
+Ask the user for an actor's first name, 
+find which actor they're referring to, 
+and print the actor's last name. (Assume 
+there's only one actor for any first name).
+=end
+
 require './connect_to_db.rb'
 
 puts "Enter a first name to look up:"
-
 first_name = readline.chomp
+
+actor = Actor.where(first_name: first_name).first
+
+if actor == nil
+puts "Sorry, #{first_name} was not found."
+else  
+puts "#{actor.first_name}'s last name is #{actor.last_name}."
+end
+
 
 # TODO: look up the last name for the given first name
 # and print something like: ___'s last name is ___.
