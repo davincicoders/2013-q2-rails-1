f16aeb0c31d496e1513991d9e792d6bdaf56735d
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-05-01 10:47:29 -0600

    FINALLY!

--- 187/187.rb
+++ 187/187.rb
@@ -1,4 +1,5 @@
 require './connect_to_db'
+require 'pry'
 
 puts "Property type (Public, Private, Parks and Rec, RTD, or leave blank):"
 property_type = readline.chomp
@@ -7,4 +8,37 @@ power_type = readline.chomp
 puts "Word to search for in station name, or leave blank:"
 word = readline.chomp
 
+#binding.pry
+
 # TODO: Implement printing out the station names that match the filters
+
+dbase= BCycleStation.order
+
+oldstation = ""
+newstation = ""
+
+if property_type != ""
+  oldstation = dbase.where("property_type like ?", "%#{property_type}%")
+end
+
+if oldstation != ""
+  if power_type != ""
+    newstation = oldstation.where("power_type like ?", "%#{power_type}%")
+    oldstation = newstation
+  end
+else
+  oldstation = dbase.where("power_type like ?", "%#{power_type}%")
+end
+
+if oldstation != ""
+  if word != ""
+    newstation = oldstation.where("station_name like ?", "%#{word}%")
+    oldstation = newstation
+  end
+else
+  oldstation = dbase.where("station_name like ?", "%#{word}%")
+end
+for i in 0..(oldstation.size - 1)
+  puts oldstation[i].station_name
+end
+#p oldstation
