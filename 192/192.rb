653ccb0a0a20256c85111824b44edabc71f86ed0
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-05-01 15:19:17 -0600

    Fudge...

--- 192/192.rb
+++ 192/192.rb
@@ -1,4 +1,5 @@
 require './connect_to_db.rb'
+require 'pry'
 
 puts "Enter a recipe name (e.g. Brownies):"
 recipe_name = readline.chomp
@@ -6,3 +7,11 @@ recipe_name = readline.chomp
 # TODO: List the names of the other recipes by this same author.
 # Don't include the recipe name that was entered at the beginning.
 # If you can't find a recipe by that name, output Unknown recipe.
+
+recipe = Recipe.where(recipe_name: recipe_name).first
+author = recipe.author
+
+recipes = author.recipes
+for recipe in recipes
+  puts recipe.recipe_name if recipe.recipe_name != recipe_name
+end
