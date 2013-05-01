d68df1b6787b7f54268a6bf77a803aed88426229
Seth Geyer <seth.geyer@gmail.com>
2013-05-01 15:34:07 -0600

    recipes

--- 192/192.rb
+++ 192/192.rb
@@ -1,7 +1,20 @@
+# Given any recipe name, list all the other recipes written by that same author.
 require './connect_to_db.rb'
 
 puts "Enter a recipe name (e.g. Brownies):"
 recipe_name = readline.chomp
+
+recipe = Recipe.where(recipe_name: recipe_name).first # pull the first object from the Recipes d'base where recipe name reflects what was input by the user.
+recipe.author_id      # Provide the author_id for the particular recipe.
+
+authors_recipes = Recipe.where(author_id: recipe.author_id)
+
+for authors_recipe in authors_recipes
+  if authors_recipe.recipe_name == recipe_name
+  else
+    puts authors_recipe.recipe_name
+  end
+end
 
 # TODO: List the names of the other recipes by this same author.
 # Don't include the recipe name that was entered at the beginning.
