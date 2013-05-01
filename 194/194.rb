570d78329dae3383ed253b213be8615c49bbd041
Daniel Stutzman <dtstutz@gmail.com>
2013-05-01 08:36:58 -0600

    Added answers for 193, 194, and 195.

--- 194/194.rb
+++ 194/194.rb
@@ -3,6 +3,19 @@ require './connect_to_db.rb'
 puts "Enter a movie title to see its awards:"
 movie_title = readline.chomp
 
+movie = Movie.where(title: movie_title).first
+if movie == nil
+  puts "Sorry, #{movie_title} was not found."
+else
+  if movie.awards.size > 0
+    for award in movie.awards
+      puts award.award_name
+    end
+  else
+    puts "This movie didn't win any awards."
+  end
+end
+
 # TODO: list the awards that this movie won, if any.
 # If it didn't won any awards, print out:
 # This movie didn't win any awards.
