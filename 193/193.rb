feee9dd04ab38676f83a4e51848d56bda9ccae65
John Walker <johnwalker@siu.edu>
2013-05-01 15:30:24 -0600

    more fun with databases.

--- 193/193.rb
+++ 193/193.rb
@@ -1,8 +1,20 @@
+# John
 require './connect_to_db.rb'
 
 puts "Enter a first name to look up:"
+first_name = readline.capitalize.chomp
 
-first_name = readline.chomp
+# define name in actor db where first names match.
+name = Actor.where(first_name: first_name).first
+
+if name == nil # if there is no match and name is empty.
+  puts "Sorry, #{first_name} was not found."
+else
+  first = name.first_name # go ahead and define first name var.
+  last = name.last_name # same with last name var.
+  puts "#{first}'s last name is #{last}."
+end
+
 
 # TODO: look up the last name for the given first name
 # and print something like: ___'s last name is ___.
