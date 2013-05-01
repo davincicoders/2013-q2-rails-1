7e7c8095ac64eefd28d32e32c8f7a06f084cb51a
Julian Hentea <redpenpress@gmail.com>
2013-05-01 06:51:31 -0700

    passed tests

--- 190/190.rb
+++ 190/190.rb
@@ -2,3 +2,13 @@ require './connect_to_db'
 
 puts "Enter a year:"
 entered_year = readline.chomp.to_i
+
+for president in President.order('id')
+president_took_office = president.took_office.split('/')[2].to_i
+president_left_office = president.left_office.split('/')[2].to_i
+
+	if entered_year >= president_took_office && entered_year <= president_left_office
+    puts president.president
+	end 
+end 
+
