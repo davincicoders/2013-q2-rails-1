7022e51a8837f2ddedd26be06e0357fb5e0c1073
John Walker <johnwalker@siu.edu>
2013-05-01 16:30:33 -0600

    capitalization issues...

--- 194/194.rb
+++ 194/194.rb
@@ -1,7 +1,26 @@
 require './connect_to_db.rb'
 
 puts "Enter a movie title to see its awards:"
+
 movie_title = readline.chomp
+
+movie = Movie.where(title: movie_title.titlecase).first
+
+if movie == nil
+  puts "Sorry, #{movie_title} was not found."
+  else
+    id = movie.id
+    awards = Award.where(movie_id: id).all
+    if awards == []
+      puts "This movie didn't win any awards."
+    else
+      for award in awards
+      puts award.award_name
+      end
+    end
+        
+
+end
 
 # TODO: list the awards that this movie won, if any.
 # If it didn't won any awards, print out:
