070f866deeb4eea1f0a46fcc57bc6ab34edcfa81
Seth Geyer <seth.geyer@gmail.com>
2013-05-01 18:28:46 -0600

    done

--- 196/196.rb
+++ 196/196.rb
@@ -6,3 +6,20 @@ full_name = readline.chomp
 # TODO: List the movies that the entered actor was in.
 # If the actor is not found, print:
 # Sorry, the actor ___ was not found.
+
+name_array = full_name.split(" ")                       # takes the string called "full_name", splits it into an array, and names it a variable called "name_array"
+firstname = name_array[0]                               # takes the first item from the array called "name_array", and calls it "firstname".
+
+
+actor = Actor.where(first_name: firstname).first        # searches the "Actor" table in the column called "first_name" for the "firstname" and returns the first object and names it a variable called "actor"
+
+if actor == nil                                         # if the value returned is nil.... 
+  puts "Sorry, the actor #{full_name} was not found."   # says "sorry I cant find it. 
+else                                                    # Otherwise...
+  movies = actor.movies                                 # lookup the actor in the movies table and return the movies in an array called "movies" 
+  for movie in movies                                   # for each movie in the movies array      
+    puts movie.title                                    # return the title of the movie
+  end
+end
+
+
