2df62c4bd280a8efda346db0254408ca715139d6
Daniel Stutzman <dtstutz@gmail.com>
2013-05-02 11:18:24 -0600

    Answer for 192.

--- 192/192.rb
+++ 192/192.rb
@@ -6,3 +6,14 @@ recipe_name = readline.chomp
 # TODO: List the names of the other recipes by this same author.
 # Don't include the recipe name that was entered at the beginning.
 # If you can't find a recipe by that name, output Unknown recipe.
+
+recipe = Recipe.where(recipe_name: recipe_name).first
+if recipe != nil
+  for other_recipe in recipe.author.recipes
+    if other_recipe.id != recipe.id
+      puts other_recipe.recipe_name
+    end
+  end
+else
+  puts "Unknown recipe."
+end
