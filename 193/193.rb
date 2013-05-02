d9290116e6c661b0f580085f9e665e3f5ec5b40d
***infinite recursion in variable git_username*** <***infinite recursion in variable git_email***>
2013-05-02 16:25:33 -0600

    193

--- 193/193.rb
+++ 193/193.rb
@@ -4,7 +4,9 @@ puts "Enter a first name to look up:"
 
 first_name = readline.chomp
 
-# TODO: look up the last name for the given first name
-# and print something like: ___'s last name is ___.
-# If you can't find an actor, print something like:
-# Sorry, __ was not found.
+actor = Actor.where(first_name: first_name).first
+if actor == nil
+  puts "Sorry, #{first_name} was not found."
+else
+  puts "#{first_name}'s last name is #{actor.last_name}."
+end
