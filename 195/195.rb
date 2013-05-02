260b9c0aec8a6db3074e748d759e38780effd87b
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-05-01 21:31:49 -0600

    All tests pass.

--- 195/195.rb
+++ 195/195.rb
@@ -1,4 +1,5 @@
 require './connect_to_db.rb'
+require 'pry'
 
 puts "Enter a movie title to see its actors (limited to this tiny database):"
 movie_title = readline.chomp
@@ -8,3 +9,15 @@ movie_title = readline.chomp
 # Sorry, ___ was not found.
 # If there are no actors for that movie, just print nothing.
 # Otherwise, print out the first and last name of each actor.
+
+movie = Movie.where(title: movie_title).first
+#binding.pry
+if movie
+  if movie.actors.first
+    for actor in movie.actors
+      puts "#{actor.first_name} #{actor.last_name}"
+    end
+  end
+else
+  puts "Sorry, #{movie_title} was not found."
+end
