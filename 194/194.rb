47da1f5d22d7ae1e66e30b1d9d95356e3b6be9e4
***infinite recursion in variable git_username*** <***infinite recursion in variable git_email***>
2013-05-02 16:35:37 -0600

    194

--- 194/194.rb
+++ 194/194.rb
@@ -3,8 +3,15 @@ require './connect_to_db.rb'
 puts "Enter a movie title to see its awards:"
 movie_title = readline.chomp
 
-# TODO: list the awards that this movie won, if any.
-# If it didn't won any awards, print out:
-# This movie didn't win any awards.
-# If there's no movie by that title, print out:
-# Sorry, ___ was not found.
+movie = Movie.where(title: movie_title).first
+if movie == nil
+  puts "Sorry, #{movie_title} was not found."
+else
+  if movie.awards.size > 0
+    for award in movie.awards
+      puts award.award_name
+    end
+  else
+    puts "This movie didn't win any awards."
+  end
+end
