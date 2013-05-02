65f3fcac7196032aee3463ea98a49a078b25f058
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-05-01 21:21:12 -0600

    All tests pass

--- 194/194.rb
+++ 194/194.rb
@@ -1,4 +1,5 @@
 require './connect_to_db.rb'
+require 'pry'
 
 puts "Enter a movie title to see its awards:"
 movie_title = readline.chomp
@@ -8,3 +9,17 @@ movie_title = readline.chomp
 # This movie didn't win any awards.
 # If there's no movie by that title, print out:
 # Sorry, ___ was not found.
+
+movie = Movie.where(title: movie_title).first
+
+if movie
+  if movie.awards.first
+    for award in movie.awards
+      puts award.award_name
+    end
+  else
+    puts "This movie didn't win any awards."
+  end
+else
+  puts "Sorry, #{movie_title} was not found."
+end
