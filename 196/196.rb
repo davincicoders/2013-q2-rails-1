e2188b9d4fdc8e669d61e4361a6d7f8fba5396ed
Daniel Stutzman <dtstutz@gmail.com>
2013-05-02 11:21:32 -0600

    Answer to 196.

--- 196/196.rb
+++ 196/196.rb
@@ -2,7 +2,18 @@ require './connect_to_db.rb'
 
 puts "Please enter the name of an actor (first and last name):"
 full_name = readline.chomp
+first_name = full_name.split(" ")[0]
+last_name = full_name.split(" ")[1]
 
-# TODO: List the movies that the entered actor was in.
-# If the actor is not found, print:
-# Sorry, the actor ___ was not found.
+actor = Actor.where(first_name: first_name, last_name: last_name).first
+if actor == nil
+  puts "Sorry, the actor #{full_name} was not found."
+else
+  if actor.movies.size > 0
+    for movie in actor.movies
+      puts movie.title
+    end
+  else
+    puts "Sorry, no movies recorded for this actor."
+  end
+end
