9a6d01347e3b5dc5391f28b96e38362e0ffaa7b3
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-05-01 21:38:36 -0600

    All tests pass!

--- 196/196.rb
+++ 196/196.rb
@@ -1,8 +1,24 @@
 require './connect_to_db.rb'
+require 'pry'
 
 puts "Please enter the name of an actor (first and last name):"
 full_name = readline.chomp
 
+strFirstName = full_name.split(" ").first
+strLastName = full_name.split(" ").last
+
 # TODO: List the movies that the entered actor was in.
 # If the actor is not found, print:
 # Sorry, the actor ___ was not found.
+
+actor = Actor.where(first_name: strFirstName).where(last_name: strLastName).first
+
+#binding.pry
+
+if actor
+  for movie in actor.movies
+    puts movie.title
+  end
+else
+  puts "Sorry, the actor #{full_name} was not found."
+end
