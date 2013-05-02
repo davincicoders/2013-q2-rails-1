0527adb4996da28cd39768710de357b9b359a859
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-05-01 21:07:42 -0600

    Changes made, tests pass.

--- 193/193.rb
+++ 193/193.rb
@@ -8,3 +8,14 @@ first_name = readline.chomp
 # and print something like: ___'s last name is ___.
 # If you can't find an actor, print something like:
 # Sorry, __ was not found.
+
+#actors.first_name, actors.last_name
+
+actor = Actor.where(first_name: first_name).first
+
+if actor
+  puts "#{actor.first_name}'s last name is #{actor.last_name}."
+else
+  puts "Sorry, #{first_name} was not found."
+end
+
