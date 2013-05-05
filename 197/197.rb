24a2ebfd42d17ec595d1f3a5fe2a1f12c1ff68fb
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-05-05 13:26:57 -0600

    All Tests PASS!!!!!

--- 197/197.rb
+++ 197/197.rb
@@ -1,6 +1,49 @@
 require './connect_to_db.rb'
+require 'pry'
 
 puts "Enter the first name of a sales rep:"
 sales_rep_first_name = readline.chomp
 
 # TODO: Implement the rest of GitHub challenge 197 here.
+
+#binding.pry
+
+line_item, products, i, subtot, tot, sales_rep, orders_by_rep, tot_com = [], [], 0, 0.0, 0.0, nil, nil, 0.0
+
+#Victoria
+sales_rep = G197SalesRep.where(first_name: sales_rep_first_name).first
+
+orders_by_rep = sales_rep.g197_orders.order("id") if sales_rep
+
+if sales_rep
+  for order in orders_by_rep
+    line_item[i] = "Order \##{order.id} - #{order.g197_customer.first_name} #{order.g197_customer.last_name}"
+    products[i] = []
+    for order_item in order.g197_order_items
+      products[i].push order_item.g197_product.name
+      
+      subtot += (order_item.g197_product.price_per_year.to_f)
+      subtot = subtot.round(1)
+    end
+    tot = ((sales_rep.commission_rate.to_i * 1.0) / 100) * subtot
+    tot = tot.round(2)
+
+    if products[i].first
+      products[i] = products[i].join(", ")
+      line_item[i] += " - #{products[i]}"
+    else
+      line_item[i] += " - none"
+    end
+    
+    tot_string = "%.2f" % tot
+    line_item[i] += " - $#{tot_string}"
+    puts line_item[i]
+    tot_com += tot
+    i = i + 1
+    tot = 0.0
+    subtot = 0.0
+  end
+  puts "Total commission: $#{'%.2f' % tot_com}"
+else
+  puts "Unknown sales rep."
+end
--- 197/connect_to_db.rb
+++ 197/connect_to_db.rb
@@ -13,3 +13,26 @@ ActiveRecord::Base.establish_connection(
 # To do: add Active Record class definitions for all the tables you need.
 # See computers_today_db.sql for what tables are available.
 # See 198/connect_to_db.rb and 199/connect_to_db.rb for examples.
+
+class G197OrderItem < ActiveRecord::Base
+  belongs_to :g197_product
+  belongs_to :g197_order
+end
+
+class G197Product < ActiveRecord::Base
+  has_many :g197_order_items
+end
+
+class G197Order < ActiveRecord::Base
+  has_many :g197_order_items
+  belongs_to :g197_customer
+  belongs_to :g197_sales_rep
+end
+
+class G197Customer < ActiveRecord::Base
+  has_many :g197_orders
+end
+
+class G197SalesRep < ActiveRecord::Base
+  has_many :g197_orders
+end
