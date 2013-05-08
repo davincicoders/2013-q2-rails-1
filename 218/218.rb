5546a014b53bf26d2cb490def4af3d4c72d20dd4
Student <student@student-3.local>
2013-05-08 12:37:45 -0600

    asf

--- 218/218.rb
+++ 218/218.rb
@@ -1,31 +1,23 @@
 require './connect_to_db.rb'
 
-puts "Enter your username:"
-entered_username = readline.chomp
-puts "Enter your password:"
-entered_password = readline.chomp
-
-# Secure but inefficient:
-# user = nil
-# for a_user in G218User.all
-#   if a_user.username == entered_username
-#     user = a_user
-#   end
-# end
-
-# NOT SECURE: vulnerable to SQL injection
-# user = G218User.where("username = '#{entered_username}'").first
-
-# More secure:
-user = G218User.where(username: entered_username).first
-
-# More secure:
-# user = G218User.where("username = ?", entered_username).first
-
-if user.password == entered_password
-  puts "Correct password"
-else
-  puts "Incorrect password"
+puts "Please enter a username."
+username = readline.chomp
+while G218User.where(username: username) == []
+  puts "Please enter a valid username."
+  username = readline.chomp
 end
+puts "Please enter your password."
+current_password = readline.chomp
+while G218User.where(username: username).where(password: current_password) == []
+  puts "The password you entered does not match your username."
+  current_password = readline.chomp
+end
+puts "Please enter a new password."
+new_password = readline.chomp
+G218User.where(username: username).first.password = new_password
+G218User.where(username: username).first.save
 
-#p Element.where("id = '5'); drop table elements; select (3")
+  
+
+
+
