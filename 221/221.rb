9cc559662ca9af983390d1bc53d35bb08f5db40a
Leor Manelis <leormanelis@gmail.com>
2013-05-10 09:08:43 -0600

    cats

--- 221/221.rb
+++ 221/221.rb
@@ -1,10 +1,5 @@
-require "active_record"
-require "pry"
-require "logger"
 require './connect_to_db.rb'
 
-class Cat < ActiveRecord::Base
-end
-
-# TODO: Update Fluffy's location to show that she is now "on the sofa".
-# TODO: Update Felix's location to show that he is now "under the bed".
+cat = Cat.where(name: "LeBron").first
+cat.name = "LeBron James"
+cat.save
---
+++ 222/killcats
@@ -0,0 +1,3 @@
+require './connect_to_db.rb'
+
+Cat.where(name: "LeBron James").first.destroy
