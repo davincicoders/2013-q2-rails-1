5fc59c62685e2de46979cc0899c811efcf55be18
Student <student@student-3.local>
2013-05-10 10:14:27 -0600

    voila

--- 221/221.rb
+++ 221/221.rb
@@ -6,5 +6,14 @@ require './connect_to_db.rb'
 class Cat < ActiveRecord::Base
 end
 
+fluffy = Cat.where(name: "Fluffy").first
+fluffy.location = "on the sofa"
+fluffy.save
+
+felix = Cat.where(name: "Felix").first
+felix.location = "under the bed"
+felix.save
+
+
 # TODO: Update Fluffy's location to show that she is now "on the sofa".
 # TODO: Update Felix's location to show that he is now "under the bed".
