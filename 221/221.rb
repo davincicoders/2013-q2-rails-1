0d749355c52e5fd56fa60cce77bc52b90a5c9661
Julian Hentea <redpenpress@gmail.com>
2013-05-10 06:59:18 -0700

    solved

--- 221/221.rb
+++ 221/221.rb
@@ -8,3 +8,11 @@ end
 
 # TODO: Update Fluffy's location to show that she is now "on the sofa".
 # TODO: Update Felix's location to show that he is now "under the bed".
+
+Fluffy = Cat.where(name:"Fluffy").first
+Fluffy.location = "on the sofa"
+Fluffy.save
+
+Felix = Cat.where(name: "Felix").first
+Felix.location = "under the bed"
+Felix.save
