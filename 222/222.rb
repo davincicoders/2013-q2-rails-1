48e186ca235c96358046b5fd82785cfc88b2fc56
Student <student@student-3.local>
2013-05-10 10:22:12 -0600

    voila

--- 222/222.rb
+++ 222/222.rb
@@ -3,6 +3,10 @@ require './connect_to_db.rb'
 class Cat < ActiveRecord::Base
 end
 
+felix = Cat.where(name: "Felix").first
+if felix
+felix.destroy
+end
 # TODO: Felix has been given away to a friend, so delete the record for him.
 # If there's no Felix record found, just do nothing; there shouldn't be an
 # error.
