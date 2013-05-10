c8c248f7b91c97fde076a10f5088745bded085b2
Student <student@student-3.local>
2013-05-10 12:33:29 -0600

    voila

--- 249/249.rb
+++ 249/249.rb
@@ -5,6 +5,18 @@ new_student_number = readline.chomp
 puts "Enter new student's full name:"
 new_full_name = readline.chomp
 
+new_student = Student.new
+new_student.full_name = new_full_name
+new_student.student_number = new_student_number
+
+if new_student.save
+  puts "Student added successfully!"
+else 
+  for error in new_student.errors.full_messages
+    puts error
+  end 
+end
+
 # TODO: Add this student to the students table,
 # unless the name or number are blank, or the number is already taken.
 # If the save fails, print out each reason why it failed.
