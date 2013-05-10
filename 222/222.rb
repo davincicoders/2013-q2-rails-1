7755648e1298ae9c5cdd4ed2665e3cbcf02844b8
Julian Hentea <redpenpress@gmail.com>
2013-05-10 06:59:07 -0700

    Solved

--- 222/222.rb
+++ 222/222.rb
@@ -6,3 +6,10 @@ end
 # TODO: Felix has been given away to a friend, so delete the record for him.
 # If there's no Felix record found, just do nothing; there shouldn't be an
 # error.
+
+Fluffy = Cat.where(name:"Fluffy").first
+
+if Fluffy.nil?
+else
+Fluffy.destroy
+end
