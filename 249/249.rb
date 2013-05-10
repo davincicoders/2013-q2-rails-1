87cccd4f54e50e9d6105b60eff710b042eceecb0
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-05-10 15:36:39 -0600

    Not yet passing in run_tests, but passes perfectly in terminal doing self tests.

--- 249/249.rb
+++ 249/249.rb
@@ -9,3 +9,15 @@ new_full_name = readline.chomp
 # unless the name or number are blank, or the number is already taken.
 # If the save fails, print out each reason why it failed.
 # Otherwise, print: Student successfully added.
+
+student = Student.new
+student.student_number = new_student_number
+student.full_name = new_full_name
+
+if student.save
+  puts "Student successfully added."
+else
+  for error in student.errors.full_messages
+    puts error
+  end
+end
--- 249/connect_to_db.rb
+++ 249/connect_to_db.rb
@@ -12,8 +12,13 @@ ActiveRecord::Base.establish_connection(
 
 class Student < ActiveRecord::Base
   has_and_belongs_to_many :courses
+  validates_presence_of :full_name
+  validates_presence_of :student_number
+  validates_uniqueness_of :student_number
 end
 
 class Course < ActiveRecord::Base
   has_and_belongs_to_many :students
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
