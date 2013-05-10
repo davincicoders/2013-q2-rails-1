de195a2191640e46522e43dd8f1cbb1686ab55d6
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-05-10 09:33:11 -0600

    Pass

--- 221/221.rb
+++ 221/221.rb
@@ -8,3 +8,14 @@ end
 
 # TODO: Update Fluffy's location to show that she is now "on the sofa".
 # TODO: Update Felix's location to show that he is now "under the bed".
+
+Cat.create(name: "Fluffy", location: "in a box")
+Cat.create(name: "Felix", location: "in a box")
+
+fluffy = Cat.where(name: "Fluffy").first
+fluffy.location = "on the sofa"
+fluffy.save
+
+felix = Cat.where(name: "Felix").first
+felix.location = "under the bed"
+felix.save
