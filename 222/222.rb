a7ea3c52837a158b45a42f9ded22be8154a3eaee
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-05-10 09:38:35 -0600

    Pass

--- 222/222.rb
+++ 222/222.rb
@@ -6,3 +6,6 @@ end
 # TODO: Felix has been given away to a friend, so delete the record for him.
 # If there's no Felix record found, just do nothing; there shouldn't be an
 # error.
+
+felix = Cat.where(name: "Felix").first
+felix.destroy if felix
