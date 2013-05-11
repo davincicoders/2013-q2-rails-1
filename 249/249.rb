69aacb93870d7beeee786f40161e2c30befa0762
***infinite recursion in variable git_username*** <***infinite recursion in variable git_email***>
2013-05-11 09:40:06 -0600

    voila

--- 249/249.rb
+++ 249/249.rb
@@ -5,12 +5,10 @@ new_student_number = readline.chomp
 puts "Enter new student's full name:"
 new_full_name = readline.chomp
 
-new_student = Student.new
-new_student.full_name = new_full_name
-new_student.student_number = new_student_number
+new_student = Student.new(full_name: new_full_name, student_number: new_student_number)
 
 if new_student.save
-  puts "Student added successfully!"
+  puts "Student successfully added."
 else 
   for error in new_student.errors.full_messages
     puts error
--- 250/250.rb
+++ 250/250.rb
@@ -1,7 +1,23 @@
 require './connect_to_db.rb'
 
 puts "Enter student name or number to withdraw:"
-name_or_number = readline.chomp
+entry = readline.chomp
+while entry == ''
+	puts "Enter student name or number to withdraw:"
+	entry = readline.chomp
+end
+if entry.to_i != 0
+  student = Student.where(student_number: entry.to_i).first 
+else
+  student = Student.where(full_name: entry).first 
+end
+if student == nil
+		puts "#{entry} is not registered."
+else student.destroy
+    	puts "#{entry} removed."
+end
+
+
 
 # TODO: If they enter a number, look up that student by student_number.
 #       (Hint: it's a number if you can convert it to a number and back to
