5fab39d24a60ab71a588177c97b5260b4ec69890
***infinite recursion in variable git_username*** <***infinite recursion in variable git_email***>
2013-05-11 10:22:26 -0600

    voila

--- 252/252.rb
+++ 252/252.rb
@@ -1,5 +1,14 @@
 require './connect_to_db.rb'
 
+for course in Course.order("course_number")
+	puts "#{course.course_number} #{course.name}"
+	for student in course.students
+		puts "  #{student.full_name}"
+	end
+end
+
+
+
 # TODO: List every course in the database, sorted by course_number,
 #       with each student enrolled in that course listed underneath, indented.
 # For example:
