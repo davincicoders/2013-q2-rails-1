cfe16dbe17178417f9d13494b15b8e6f7e96a8ee
Ann Karpinski <annkarpinski@gmail.com>
2013-05-13 01:29:56 -0600

    Code recognizes user input as integer or string. Program either removes existing student record or prints record does not exist.

--- 250/250.rb
+++ 250/250.rb
@@ -3,6 +3,19 @@ require './connect_to_db.rb'
 puts "Enter student name or number to withdraw:"
 name_or_number = readline.chomp
 
+if name_or_number.to_i.to_s == name_or_number
+  student = Student.where(student_number: name_or_number).first
+else 
+  student = Student.where(full_name: name_or_number).first
+end
+ 
+if student != nil
+  student.destroy
+  puts "#{name_or_number} removed."
+else
+  puts "#{name_or_number} is not registered."
+end
+
 # TODO: If they enter a number, look up that student by student_number.
 #       (Hint: it's a number if you can convert it to a number and back to
 #        a string and it's still the same).
