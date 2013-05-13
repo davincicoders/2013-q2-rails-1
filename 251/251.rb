8b3046ed7bf51e8305e515975f4bc3d54663cd6c
Ann Karpinski <annkarpinski@gmail.com>
2013-05-13 02:55:00 -0600

    Program checks for existence of course and then student. If both exist, the student is added to the course.

--- 251/251.rb
+++ 251/251.rb
@@ -6,10 +6,25 @@ course_num = readline.chomp
 # TODO: Look up the course by its number.  If there's no such course, print:
 # "No such course." and exit.
 
-puts "Enter the student number:"
-student_num = readline.chomp
+course = Course.where(course_number: course_num).first
+if course == nil
+  puts "No such course."
+end
 
 # TODO: Look up the student by their number.  If there's no such student, print:
 # "No such student." and exit.
 
+if course != nil
+  puts "Enter the student number:"
+  student_num = readline.chomp
+  student = Student.where(student_number: student_num).first
+  if student == nil
+    puts "No such student."
+  end
+end  
+
 # TODO: add that student to that course.
+
+if course != nil && student != nil
+  course.students << student
+end
