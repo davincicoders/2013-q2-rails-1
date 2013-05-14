5751c7ad8dc16fb7f5a1b0be7872f824b9ee8579
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-05-14 16:47:06 -0600

    Added commented instructions for reference.

---
+++ 262/262.rb
@@ -0,0 +1,9 @@
+=begin
+Directory 262: Write a web app to show all the orders for the Computers Today business (same data as directory 197). The columns should be:
+  Order ID
+  Customer name (not customer ID)
+  Sales rep name (not sales rep ID)
+  Date received
+  Payment method
+  Product names (join through both g197_order_items and g197_products)
+=end
