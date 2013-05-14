2b8e343eddba33c5fad23116bed12af3d99daeda
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-05-14 16:54:33 -0600

    I: Oops!  Instinctively began to edit 262 as the project code, when it's supposed to be the server! R: Instructions should be moved to the project code file. A: Created views folder, orders.erb inside views folder, cut & pasted instructions.  Created server code.

--- 262/262.rb
+++ 262/262.rb
@@ -1,9 +1,7 @@
-=begin
-Directory 262: Write a web app to show all the orders for the Computers Today business (same data as directory 197). The columns should be:
-  Order ID
-  Customer name (not customer ID)
-  Sales rep name (not sales rep ID)
-  Date received
-  Payment method
-  Product names (join through both g197_order_items and g197_products)
-=end
+require 'sinatra'
+require './connect_to_db.rb'
+
+get("/") do
+  @orders = G197Order.all
+  halt erb(:orders)
+end
