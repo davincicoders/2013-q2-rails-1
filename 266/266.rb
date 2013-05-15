33484461f45b44ff3c32025b4a43dea8c9d8cb39
Seth Geyer <seth.geyer@gmail.com>
2013-05-15 16:31:22 -0600

    Updates to link to and build an orders page.

--- 266/266.rb
+++ 266/266.rb
@@ -6,5 +6,14 @@ get("/orders") do
   halt erb(:orders)
 end
 
+
+get("/order/:order_number") do
+  order_no = params["order_number"]
+  @order_no = G197Order.where(id: order_no).first.
+  halt erb(:order)
+end
+
+
+
 # TODO: Write a GET handler for paths like /order/1 and /order2.
 # It should render an ERb file to look like order-mock-up.html
