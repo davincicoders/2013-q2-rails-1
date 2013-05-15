89e5767c06f15d81a6777dce6e789846a368962f
John Walker <johnwalker@siu.edu>
2013-05-15 17:20:27 -0600

    Homework for 267. New property, properties, 267.rb and connect to db files.

---
+++ 267/267.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require './connect_to_db.rb'
+
+
+get("/") do
+  @properties = Properties.order("id")
+  halt erb(:properties)
+end
+
+get("/property/:id") do
+  id = params["id"]
+  @property = Properties.where(id: id).first
+
+  if @property != nil
+    halt erb(:property)
+  else
+    halt 404, "The requested property not found"
+  end
+end
