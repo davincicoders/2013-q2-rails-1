5fc77ae60edb17073a56afc1fcda5359bc2c4793
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-05-15 21:16:26 -0600

    Finished assignment..

--- 266/266.rb
+++ 266/266.rb
@@ -8,3 +8,12 @@ end
 
 # TODO: Write a GET handler for paths like /order/1 and /order2.
 # It should render an ERb file to look like order-mock-up.html
+
+get("/order/order?:number?") do
+  number = params["number"] #WORKS!! Returns number asked for in URL.  If /order2, returns 2. order3 returns 3, etc.
+  
+  @order = G197Order.where(id: number).first
+  @order_num = number #Totally un-necissary, but good to know!
+  
+  halt erb(:order)
+end
