3ba9bb1f1beaac023508982f8e51db8a2ebafe7f
Student <student@student-3.local>
2013-05-17 10:19:03 -0600

    indentation corrected

--- 264/prime.rb
+++ 264/prime.rb
@@ -1,26 +1,26 @@
-def prime num
+def prime(num)
   primes = []
   i = 2
-		while (i / 2) < 2
-			primes.push(i)
-			i += 1
-			break if primes.size == num
-		end
-		until primes.size == num
-		  2.upto(Math.sqrt(i).to_i) do |x|
-			  if x == Math.sqrt(i).to_i && i % x != 0
-			  	primes.push(i)
-			  	i += 1
-			  	break
-			  elsif i % x != 0
-			  	next
-			  else
-				i += 1 
-				break
-			  end
+  while (i / 2) < 2
+	  primes.push(i)
+	  i += 1
+	  break if primes.size == num
+	end
+	until primes.size == num
+		2.upto(Math.sqrt(i).to_i) do |x|
+			if x == Math.sqrt(i).to_i && i % x != 0
+        primes.push(i)
+        i += 1
+        break
+      elsif i % x != 0
+        next
+      else
+      i += 1 
+      break
+      end
 		  end
 		end
 	puts primes[-1]
 end
 
-prime 10000
+prime 10001
