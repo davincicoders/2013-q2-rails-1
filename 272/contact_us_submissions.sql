cd6f95ccb9256550a11a918587608c09c5593dd4
Leor Manelis <leormanelis@gmail.com>
2013-05-17 14:43:49 -0600

    made a form but it still not connecting to my database

--- 272/contact_us_submissions.sql
+++ 272/contact_us_submissions.sql
@@ -1,3 +1,15 @@
 drop table if exists contact_us_submissions;
 
--- todo: write create table
+
+create table contact_us_submissions (
+id		       serial,
+your_name    varchar(100),
+email		     varchar(100),
+web_site	   varchar(100),
+country      varchar(100),
+human_check  boolean,
+comments     varchar(500),
+primary key        (ID)
+
+);
+
