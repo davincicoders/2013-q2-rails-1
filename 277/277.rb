b45b864f2432b5c9ee76e6edce9a74ff778ba9b1
Daniel Stutzman <dtstutz@gmail.com>
2013-05-18 18:14:29 -0600

    Added 277: combined GET and POST ERb files.

---
+++ 277/277.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'sinatra_more/markup_plugin'
+require './connect_to_db.rb'
+
+register SinatraMore::MarkupPlugin
+
+get "/" do
+  redirect "/contact_us"
+end
+
+get "/contact_us" do
+  @message = ContactUsMessage.new
+  halt erb(:contact_us)
+end
+
+post "/contact_us" do
+  @message         = ContactUsMessage.new
+  @message.name    = params["name"]
+  @message.email   = params["email"]
+  @message.message = params["message"]
+
+  if @message.save
+    redirect "/thank_you"
+  else
+    halt erb(:contact_us)
+  end
+end
+
+get "/thank_you"  do
+  halt erb(:thank_you)
+end
