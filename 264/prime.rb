6a1e059cf166177d71f2f16be1354dec50540b31
***infinite recursion in variable git_username*** <***infinite recursion in variable git_email***>
2013-05-19 10:28:44 -0600

    voila

--- 264/prime.rb
+++ 264/prime.rb
@@ -1,17 +1,11 @@
 def prime(num)
-  primes = []
-  i = 2
-  while (i / 2) < 2
-	  primes.push(i)
-	  i += 1
-	  break if primes.size == num
-	end
-	until primes.size == num
-		2.upto(Math.sqrt(i).to_i) do |x|
+  primes = [2, 3]
+  i = 4
+  until primes.size == num
+		2.upto(Math.sqrt(i).to_i) do |x| 
 			if x == Math.sqrt(i).to_i && i % x != 0
         primes.push(i)
         i += 1
-        break
       elsif i % x != 0
       else
       i += 1 
