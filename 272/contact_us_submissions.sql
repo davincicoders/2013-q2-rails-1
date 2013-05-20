8bc30d9ab9f281cb795f13eff9d5a03c2b88ba93
Julian Hentea <redpenpress@gmail.com>
2013-05-20 08:57:17 -0700

    homework one the weekend

--- 272/contact_us_submissions.sql
+++ 272/contact_us_submissions.sql
@@ -1,3 +1,11 @@
 drop table if exists contact_us_submissions;
 
--- todo: write create table
+create table contact_us_submissions (
+  id serial,
+  your_name varchar(250),
+  email   varchar(250),
+  website    varchar(250),
+  country   boolean,
+  comments  varchar(250),
+  primary key (id)
+);
