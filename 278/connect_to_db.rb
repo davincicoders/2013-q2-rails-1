7ef990752a2a4bb65a99a87151bf760f4d77eec0
Seth Geyer <seth.geyer@gmail.com>
2013-05-20 13:02:36 -0600

    posted

--- 278/connect_to_db.rb
+++ 278/connect_to_db.rb
@@ -11,4 +11,7 @@ ActiveRecord::Base.establish_connection(
   database: "postgres")
 
 class ContactUsComment < ActiveRecord::Base
+validates_presence_of :name
+validates_presence_of :email
+validates_presence_of :comments 
 end
