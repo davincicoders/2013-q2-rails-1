a0747a7ec6319f4f8124f2bab959d467ef7d1224
Seth Geyer <seth.geyer@gmail.com>
2013-05-19 20:53:50 -0600

    done

--- 273/273.rb
+++ 273/273.rb
@@ -9,6 +9,26 @@ get("/") do
 end
 
 post("/handle_post") do
+  new = SubOrder.new
+  new.sandwich_type = params["sandwich_type"]
+  new.bread_type = params["bread_type"]
+  new.want_12_inch = params["upgrade"]
+  new.want_extra_cheese = params["extra"]
+  new.want_chips = params["add"]
+  money = 3.49 
+    if params["upgrade"]
+      money = money + 2.00
+    end
+    if params["extra"]
+      money = money + 0.49
+    end
+    if params["add"]
+      money = money + 0.99
+    end
+    new.total_order = money.round(2)
+  
+    new.save!
+  
   # TODO: save the sub order into the sub_orders table.
   # Make sure to fill out:
   # - sandwich_type (string)
