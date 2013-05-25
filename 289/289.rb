9734f4605c110dfcc1d5f00bd71facd948ba34e9
Daniel Stutzman <dtstutz@gmail.com>
2013-05-25 15:09:17 -0600

    Added exercises 287, 288, and 289 (bank app).

---
+++ 289/289.rb
@@ -0,0 +1,50 @@
+require 'sinatra'
+require 'json'
+require './connect_to_db.rb'
+
+use Rack::Session::Cookie, secret: SecureRandom.hex
+after { ActiveRecord::Base.connection.close } # fix ConnectionTimeoutError
+
+SAFE_PAGES = ["/", "/login", "/logout"]
+before do
+  @bank_user = BankUser.where(id: session["bank_user_id"]).first
+  if !SAFE_PAGES.include?(request.path_info) && @bank_user == nil
+    redirect "/login"
+  end
+end
+
+get "/"  do
+  redirect "/login"
+end
+
+get "/login"  do
+  halt erb(:login, layout: false)
+end
+
+post "/login" do
+  found_bank_user = BankUser.where(username: params["username"]).first
+  if found_bank_user && params["password"] == found_bank_user.password
+    session["bank_user_id"] = found_bank_user.id
+    redirect "/accounts"
+  else
+    halt erb(:login, layout: false)
+  end
+end
+
+get "/accounts" do
+raise
+  halt erb(:accounts)
+end
+
+get "/location" do
+  halt erb(:location)
+end
+
+get "/rates" do
+  halt erb(:rates)
+end
+
+get "/logout" do
+  session.clear
+  redirect "/login"
+end
