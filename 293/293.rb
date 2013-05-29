c0a7e4fae79b3964d2d1c9695e81b92ad512f8ec
Daniel Stutzman <dtstutz@gmail.com>
2013-05-29 16:48:48 -0600

    Answer to 293.

--- 293/293.rb
+++ 293/293.rb
@@ -15,18 +15,40 @@ get "/plots" do
 end
 
 post "/plots" do
-  # TODO: if a garden plot was deleted, delete it
+  if params["plot_id_to_delete"] != nil
+    plot = GardenPlot.where(id: params["plot_id_to_delete"]).first
+    plot.destroy
+  end
   redirect "/plots"
 end
 
 get "/plots/:plot_id" do
-  # TODO: set @title to Create New Plot or Edit Plot, depending on plot_id
-  # TODO: find @plot (based on plot_id, which could be new)
+  if params["plot_id"] == "new"
+    @title = "Create new plot"
+    @plot = GardenPlot.new
+  else
+    @title = "Edit plot"
+    @plot = GardenPlot.where(id: params["plot_id"]).first
+  end
   halt erb(:garden_plot)
 end
 
 post "/plots/:plot_id" do
-  # TODO: find @plot (based on plot_id, which could be new)
-  # TODO: set fields on @plot based on the form params
-  # TODO: attempt to save, then either redirect or show the form with errors
+  if params["plot_id"] == "new"
+    @plot = GardenPlot.new
+  else
+    @plot = GardenPlot.where(id: params["plot_id"]).first
+  end
+
+  @plot.seed_type = params["seed_type"]
+  @plot.planted_year = params["planted_year"]
+  @plot.planted_month = params["planted_month"]
+  @plot.planted_day = params["planted_day"]
+  @plot.is_unused = params["is_unused"]
+
+  if @plot.save
+    redirect "/plots"
+  else
+    halt erb(:garden_plot)
+  end
 end
