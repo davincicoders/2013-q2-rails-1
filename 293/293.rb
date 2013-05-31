4e0417ac1556de28b6b50317959de3e6d81d8408
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-05-31 11:36:20 -0600

    Yay.

--- 293/293.rb
+++ 293/293.rb
@@ -20,13 +20,41 @@ post "/plots" do
 end
 
 get "/plots/:plot_id" do
-  # TODO: set @title to Create New Plot or Edit Plot, depending on plot_id
-  # TODO: find @plot (based on plot_id, which could be new)
+  if params["plot_id"] == "new"
+    @title = "Create New Plot"
+    @plot = GardenPlot.new
+  else
+    @title = "Edit Plot"
+    @plot = GardenPlot.where(id: params["plot_id"]).first
+  end
   halt erb(:garden_plot)
 end
 
 post "/plots/:plot_id" do
-  # TODO: find @plot (based on plot_id, which could be new)
-  # TODO: set fields on @plot based on the form params
-  # TODO: attempt to save, then either redirect or show the form with errors
+  if params["plot_id"] == "new"
+    @plot = GardenPlot.new
+    @plot.seed_type = params["seed_type"]
+    @plot.planted_year = params["planted_year"]
+    @plot.planted_month = params["planted_month"]
+    @plot.planted_day = params["planted_day"]
+    @plot.is_unused = (params["is_unused"] != nil)
+    if @plot.save
+      redirect "/plots"
+    else
+      halt erb(:garden_plot)
+    end
+  else
+    raise params.inspect
+    @plot = GardenPlot.where(id: params["plot_id"]).first
+    @plot.seed_type = params["seed_type"]
+    @plot.planted_year = params["planted_year"]
+    @plot.planted_month = params["planted_month"]
+    @plot.planted_day = params["planted_day"]
+    @plot.is_unused = (params["is_unused"] != nil)
+    if @plot.save
+      redirect "/plots"
+    else
+      halt erb(:garden_plot)
+    end
+  end
 end
