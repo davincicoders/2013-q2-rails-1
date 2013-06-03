acfb6dcb3a6da55f3ce6125c890f08a29c93c3e4
John Walker <johnwalker@siu.edu>
2013-06-03 09:06:46 -0600

    x

--- 285/285.rb
+++ 285/285.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra_more/markup_plugin'
 require './connect_to_db.rb'
+require 'pry'
 
 register SinatraMore::MarkupPlugin
 
@@ -18,12 +19,27 @@ get("/table/:table_id") do
 end
 
 post("/table/:table_id") do
+  binding.pry
   @table = RestaurantTable.where(id: params["table_id"]).first
   @order = RestaurantOrder.where(
     is_closed: false, restaurant_table_id: @table.id).first
   if @order.nil?
+<<<<<<< Updated upstream
     # TODO: mark a table as occupied, complaining if there aren't
     #       enough seats for the party.
+=======
+    if params["party_size"].to_i > @table.number_of_chairs
+      @error = "Table only has #{@table.number_of_chairs} chairs"
+      halt erb(:show_table)
+    else
+      @order = RestaurantOrder.new
+      @order.party_size          = params["party_size"]
+      @order.restaurant_table_id = params["table_id"]
+      @order.is_closed           = false
+      @order.save!
+    end
+  
+>>>>>>> Stashed changes
   elsif params["action"] == "add_item_to_order"
     # TODO: add an item to the order, complaining if an item or
     #       chair number wasn't picked.
