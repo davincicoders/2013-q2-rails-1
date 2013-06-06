eefea79b5046e6b2e3af553fb65ea2b1c323f79d
Daniel Stutzman <dtstutz@gmail.com>
2013-06-06 16:54:17 -0600

    Changes needed to deploy to Heroku.

--- 303/connect_to_db.rb
+++ 303/connect_to_db.rb
@@ -1,14 +1,27 @@
 require 'active_record'
 require 'logger'
 
-#ActiveRecord::Base.logger = Logger.new(STDOUT)
-#ActiveRecord::Base.logger.formatter = proc { |sev, time, prog, msg| "#{msg}\n" }
-ActiveRecord::Base.establish_connection(
-  host:     "localhost",
-  adapter:  "postgresql",
-  username: "postgres",
-  password: "postgres",
-  database: "postgres")
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+ActiveRecord::Base.logger.formatter = proc { |sev, time, prog, msg| "#{msg}\n" }
+if ENV['DATABASE_URL'] # for Heroku
+  db = URI.parse(ENV['DATABASE_URL'])
+  ActiveRecord::Base.establish_connection({
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :port     => db.port,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8',
+  })
+else # for local development
+  ActiveRecord::Base.establish_connection(
+    host:     "localhost",
+    adapter:  "postgresql",
+    username: "postgres",
+    password: "postgres",
+    database: "postgres")
+end
 
 class Car < ActiveRecord::Base
   belongs_to :car_share_member
