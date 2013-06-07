69b2c3d742aa0bbe0f921a84a77b652f5de65ff9
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-06-07 12:37:49 -0600

    Done.

--- 302/302.rb
+++ 302/302.rb
@@ -1,9 +1,13 @@
 require 'sinatra'
 require 'sinatra_more/markup_plugin'
 require './connect_to_db.rb'
+require 'rack-flash'
 
 register SinatraMore::MarkupPlugin
 after { ActiveRecord::Base.connection.close } # fix ConnectionTimeoutError
+use Rack::Session::Cookie, secret: SecureRandom.hex
+
+use Rack::Flash, sweep: true
 
 get "/"  do
   redirect "/plots"
@@ -18,6 +22,7 @@ post "/plots" do
   if params["plot_id_to_delete"] != nil
     plot = GardenPlot.where(id: params["plot_id_to_delete"]).first
     plot.destroy
+    flash[:confirm] = "Deletion Confirmed!"
   end
   redirect "/plots"
 end
@@ -35,8 +40,12 @@ end
 
 post "/plots/:plot_id" do
   if params["plot_id"] == "new"
+    flash_type = :create
+    flash_message = "Created new record!"
     @plot = GardenPlot.new
   else
+    flash_type = :change
+    flash_message = "Changed record!"
     @plot = GardenPlot.where(id: params["plot_id"]).first
   end
 
@@ -47,8 +56,10 @@ post "/plots/:plot_id" do
   @plot.is_unused = params["is_unused"]
 
   if @plot.save
+    flash[flash_type] = flash_message
     redirect "/plots"
   else
+    flash[:error] = "Could not save!"
     halt erb(:garden_plot)
   end
 end
