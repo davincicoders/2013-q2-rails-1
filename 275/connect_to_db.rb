4870db288d9c9688778d352af65bb9c5214630d4
Daniel Stutzman <dtstutz@gmail.com>
2013-06-08 15:21:22 -0600

    Allow all example directories to connect to Heroku's database instead of only localhost.

--- 275/connect_to_db.rb
+++ 275/connect_to_db.rb
@@ -3,12 +3,25 @@ require 'logger'
 
 #ActiveRecord::Base.logger = Logger.new(STDOUT)
 #ActiveRecord::Base.logger.formatter = proc { |sev, time, prog, msg| "#{msg}\n" }
-ActiveRecord::Base.establish_connection(
-  host:     "localhost",
-  adapter:  "postgresql",
-  username: "postgres",
-  password: "postgres",
-  database: "postgres")
+if ENV['DATABASE_URL']
+  db = URI.parse(ENV['DATABASE_URL'])
+  ActiveRecord::Base.establish_connection({
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :port     => db.port,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8',
+  })
+else
+  ActiveRecord::Base.establish_connection(
+    host:     "localhost",
+    adapter:  "postgresql",
+    username: "postgres",
+    password: "postgres",
+    database: "postgres")
+end
 
 class ContactUsMessage < ActiveRecord::Base
 end
