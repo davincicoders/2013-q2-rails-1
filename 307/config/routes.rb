e502a26458fdb90b293e78aba5e9c921dffa1b18
Julian Hentea <redpenpress@gmail.com>
2013-06-10 12:32:20 -0700

    App working

--- 307/config/routes.rb
+++ 307/config/routes.rb
@@ -1,4 +1,10 @@
 App::Application.routes.draw do
+  
+  get  "/"              => redirect("/credit_card")
+  get  "/credit_card"   => "cart#edit_credit_card"
+  post "/credit_card"   => "cart#update_credit_card"
+  get  "/thank_you"     => "cart#thank_you"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
