1e91e6ffea6b787771096c790ed83fcd24923243
Leor Manelis <leormanelis@gmail.com>
2013-06-10 12:11:41 -0600

    new changes

---
+++ 307/app/controllers/cart_controller.rb
@@ -0,0 +1,31 @@
+class CartController < ApplicationController
+
+  get "/" do
+    redirect "/credit_card"
+  end
+
+  get "/credit_card" do
+    @credit_card = CreditCard.new
+    halt erb(:credit_card)
+  end
+  
+get "/credit_card" => "cart#edit_credit_card"
+
+  post "/credit_card" do
+    @credit_card = CreditCard.new
+    @credit_card.card_type           = params[:card_type]
+    @credit_card.number              = params[:number]
+    @credit_card.verification_number = params[:verification_number]
+    @credit_card.expiration_month    = params[:expiration_month]
+    @credit_card.expiration_year     = params[:expiration_year]
+  
+    if @credit_card.save
+      redirect "/thank_you"
+    else
+      halt erb(:credit_card)
+    end
+  end
+end
+get "/thank_you" do
+  halt erb(:thank_you)
+end
