b2bc6c44c769e4ec57f3f35ea16f9e4310cf823a
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-06-10 13:52:26 -0600

    Created 'update' definition in cart controller

--- 307/app/controllers/cart_controller.rb
+++ 307/app/controllers/cart_controller.rb
@@ -6,4 +6,21 @@ class CartController < ApplicationController
     @credit_card = CreditCard.new
     render :edit_credit_card and return
   end
+  def update
+    @credit_card = CreditCard.new
+    @credit_card.card_type           = params[:card_type]
+    @credit_card.number              = params[:number]
+    @credit_card.verification_number = params[:verification_number]
+    @credit_card.expiration_month    = params[:expiration_month]
+    @credit_card.expiration_year     = params[:expiration_year]
+    
+    if @credit_card.save
+      redirect_to "/thank_you" and return
+    else
+      render :edit_credit_card and return
+    end
+  end
+  def thank_you
+    render :thank_you and return
+  end
 end
