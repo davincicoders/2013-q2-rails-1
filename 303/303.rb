f71c242a25bb70d9f30846111cc1e5e7725c50f4
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-06-10 09:45:09 -0600

    Cars

--- 303/303.rb
+++ 303/303.rb
@@ -9,3 +9,41 @@ use Rack::Session::Cookie, secret: SecureRandom.hex
 use Rack::Flash, sweep: true
 
 # TODO: write request handlers below
+
+get "/" do
+  redirect "/login"
+end
+
+get "/login" do
+  halt erb(:login)
+end
+
+post "/login" do
+  user = CarShareMember.where(username: params["username"]).first
+  if user && user.authenticate(params["password"])
+    session["user_id"] = user.id
+    redirect "/reservations"
+  else
+    flash[:error] = "Unknown username or wrong password."
+    halt erb(:login)
+  end
+end
+
+get "/reservations" do
+  @user = CarShareMember.where(id: session["user_id"]).first
+  @cars = Car.all
+  @car = Car.where(car_share_member_id: session["user_id"]).first
+  halt erb(:reservations)
+end
+
+post "/reservations" do
+  
+  halt erb(:reservations)
+end
+
+post "/return" do
+  car = Car.where(car_share_member_id: session["user_id"]).first
+  car.car_share_member_id = ""
+  car.save
+  halt erb(:reservations)
+end
