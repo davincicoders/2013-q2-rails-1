0054add94a265961849c3aa2fd5b838ab69c8b35
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-06-10 10:15:08 -0600

    Finished!

--- 303/303.rb
+++ 303/303.rb
@@ -10,6 +10,15 @@ use Rack::Flash, sweep: true
 
 # TODO: write request handlers below
 
+SAFE_PAGES = ["/", "/login"]
+before do
+  @user = CarShareMember.where(id: session["user_id"]).first
+  if !SAFE_PAGES.include?(request.path_info) && @user == nil
+    flash[:error] = "Please log in."
+    redirect "/login"
+  end
+end
+
 get "/" do
   redirect "/login"
 end
@@ -31,19 +40,29 @@ end
 
 get "/reservations" do
   @user = CarShareMember.where(id: session["user_id"]).first
-  @cars = Car.all
+  @cars = Car.order("id")
   @car = Car.where(car_share_member_id: session["user_id"]).first
   halt erb(:reservations)
 end
 
 post "/reservations" do
-  
-  halt erb(:reservations)
-end
-
-post "/return" do
-  car = Car.where(car_share_member_id: session["user_id"]).first
-  car.car_share_member_id = ""
-  car.save
-  halt erb(:reservations)
+  @user = CarShareMember.where(id: session["user_id"]).first
+  @cars = Car.order("id")
+  @car = Car.where(car_share_member_id: session["user_id"]).first
+  if params["logout"] != nil
+    session.clear
+    redirect "/login"
+  end
+  if params["return_this_car"] != nil
+    ret_car = Car.where(car_share_member_id: session["user_id"]).first
+    ret_car.car_share_member_id = ""
+    ret_car.save
+    redirect "/reservations"
+  end
+  if params["reserve_this_car"] != nil
+    reserve_car = Car.where(id: params["reserve_this_car"]).first
+    reserve_car.car_share_member_id = session["user_id"]
+    reserve_car.save
+    redirect "/reservations"
+  end
 end
