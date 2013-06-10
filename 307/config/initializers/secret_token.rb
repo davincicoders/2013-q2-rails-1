3288f13442339bd315cf85c59d95248ea19d0781
John Walker <johnwalker@siu.edu>
2013-06-10 11:40:00 -0600

    pushing base ruby project.

---
+++ 307/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = 'fc4f0ad6726b9b49b1dec34ddff1b1f999db6af63916e4eb72ea55ca7362bd23428e571e418c6d970db9fc0a6af16a2149c29b7619e0cd9e483e4a2639d6c6af'
