2c41a293d3c531872c7492c34004cace51fec97c
John Walker <johnwalker@siu.edu>
2013-06-10 12:41:15 -0600

    First rails app. simple credit card login thingy.

--- 307/config/routes.rb
+++ 307/config/routes.rb
@@ -1,4 +1,10 @@
 App::Application.routes.draw do
+  get "/"             => redirect("/credit_card")
+  get "/credit_card"  => "main#edit_credit_card"
+  post "/credit_card" => "main#update_credit_cart"
+  get "/thank_you"    => "main#thank_you"
+end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +61,3 @@ App::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
