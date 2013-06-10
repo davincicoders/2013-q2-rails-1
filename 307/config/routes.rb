cf453fb5ffcd98b8c9b58e1ab89807ece7c4a817
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-06-10 13:53:43 -0600

    Removed sinatra route handelers previously commented out.

--- 307/config/routes.rb
+++ 307/config/routes.rb
@@ -1,33 +1,6 @@
 App::Application.routes.draw do
-  #get "/" do
-  #  redirect "/credit_card"
-  #end
-  
   get "/" => "cart#root"
-  
-  #get "/credit_card" do
-  #  @credit_card = CreditCard.new
-  #  halt erb(:credit_card)
-  #end
-
   get "/credit_card" => "cart#edit_credit_card"
-
-  #post "/credit_card" do
-  #  @credit_card = CreditCard.new
-  #  @credit_card.card_type           = params[:card_type]
-  #  @credit_card.number              = params[:number]
-  #  @credit_card.verification_number = params[:verification_number]
-  #  @credit_card.expiration_month    = params[:expiration_month]
-  #  @credit_card.expiration_year     = params[:expiration_year]
-    
-  #  if @credit_card.save
-  #    redirect "/thank_you"
-  #  else
-  #    halt erb(:credit_card)
-  #  end
-  #end
-
-  #get "/thank_you" do
-  #  halt erb(:thank_you)
-  #end
+  post "/credit_card" => "cart#update"
+  get "/thank_you" => "cart#thank_you"
 end
