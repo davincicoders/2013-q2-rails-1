58c6c87bbf22a6927dc5b9e25ac9713684729dec
Leor Manelis <leormanelis@gmail.com>
2013-06-11 10:47:15 -0600

    changed get and post routes to proper ruby code. line 6, 12, 28

--- 307/config/routes.rb
+++ 307/config/routes.rb
@@ -1,6 +1,31 @@
 App::Application.routes.draw do
 
+  #get "/" do
+  #  redirect "/credit_card"
+  #end
+get "/" => "cart#root"
 
+  #get "/credit_card" do
+   # @credit_card = CreditCard.new
+    #halt erb(:credit_card)
+  #end
+get "/credit_card" => "cart#edit_credit_card"
+
+  #post "/credit_card" do
+   # @credit_card = CreditCard.new
+    #@credit_card.card_type           = params[:card_type]
+    #@credit_card.number              = params[:number]
+    #@credit_card.verification_number = params[:verification_number]
+    #@credit_card.expiration_month    = params[:expiration_month]
+    #@credit_card.expiration_year     = params[:expiration_year]
+  
+    #if @credit_card.save
+    #  redirect "/thank_you"
+    #else
+    #  halt erb(:credit_card)
+    #end
+  #end
+post "/credit_card" => "cart#update"
 end
   # The priority is based upon order of creation:
   # first created -> highest priority.
