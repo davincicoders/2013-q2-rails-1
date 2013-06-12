aa2f92dc2701de545b571cb7040cce62cb84af03
Daniel Stutzman <dtstutz@gmail.com>
2013-06-11 22:13:53 -0600

    Converted restaurant app from 313 to Rails app in 315.

--- 315/db/seeds.rb
+++ 315/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+MenuItem.create(name: 'Hot Chocolate', price: 2.50)
+MenuItem.create(name: 'Milk',          price: 2.00)
+MenuItem.create(name: 'Bottle water',  price: 2.00)
+MenuItem.create(name: 'Juice',         price: 2.50)
+MenuItem.create(name: 'Canned pop',    price: 2.00)
+
+RestaurantTable.create(id:  1, number_of_chairs: 2)
+RestaurantTable.create(id:  2, number_of_chairs: 2)
+RestaurantTable.create(id:  3, number_of_chairs: 2)
+RestaurantTable.create(id:  4, number_of_chairs: 2)
+RestaurantTable.create(id:  5, number_of_chairs: 2)
+RestaurantTable.create(id:  6, number_of_chairs: 2)
+RestaurantTable.create(id:  7, number_of_chairs: 2)
+RestaurantTable.create(id:  8, number_of_chairs: 4)
+RestaurantTable.create(id:  9, number_of_chairs: 2)
+RestaurantTable.create(id: 10, number_of_chairs: 4)
+RestaurantTable.create(id: 11, number_of_chairs: 2)
