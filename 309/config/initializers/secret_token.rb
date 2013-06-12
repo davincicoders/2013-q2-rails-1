2aa3fa2ead252b62f9286bbac69bbf1ead976af7
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-06-12 11:02:26 -0600

    Created 309 rails project

---
+++ 309/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = 'a28f49bb65871de9fb7435f9581f3568dc1b70cbf757a124296d970066867c090b62a51e11a7afba71571311e06a90505fa993e5296392d270ae9067193c0a32'
