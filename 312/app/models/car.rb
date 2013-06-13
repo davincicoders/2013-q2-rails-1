fafa629cbc8dd139ad308e662f582ea404a01dbf
Josh Langford <josh@josh-Latitude-E6420.(none)>
2013-06-12 19:26:04 -0600

    Fin!

--- 312/app/models/car.rb
+++ 312/app/models/car.rb
@@ -1,3 +1,4 @@
 class Car < ActiveRecord::Base
   belongs_to :car_share_member
+  attr_accessible :color, :model, :car_share_member_id
 end
--- 312/db/seeds.rb
+++ 312/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Car.create(color: "red", model: "VW Golf", car_share_member_id: nil)
+Car.create(color: "black", model: "VW Jetta", car_share_member_id: 2)
+Car.create(color: "white", model: "Toyota Celica", car_share_member_id: nil)
+
+CarShareMember.create(first_name: "Jen", username: "Jen", password_digest: "$2a$10$a4wQUbztQI9QIjgKaSBFcOiW.qVyTAD4aybmlRbXlgd9E3er5I/6.")
+CarShareMember.create(first_name: "Ted", username: "Ted", password_digest: "$2a$10$awwXyeevdXJdmxU6zzrFCOJwB/AZJkszAHx2EgmxXGgx8UDX1Y0T2")
