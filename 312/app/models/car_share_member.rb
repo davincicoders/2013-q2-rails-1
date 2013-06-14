9c104103a5d159b6bb73120ef05d580cd7def8e8
***infinite recursion in variable git_username*** <***infinite recursion in variable git_email***>
2013-06-13 21:07:03 -0600

    asdf

--- 312/app/models/car_share_member.rb
+++ 312/app/models/car_share_member.rb
@@ -1,4 +1,5 @@
 class CarShareMember < ActiveRecord::Base
   has_many :cars
   has_secure_password
+  attr_accessible :first_name, :username, :password_digest
 end
--- 312/db/seeds.rb
+++ 312/db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'bcrypt'
+
+Car.create(color: 'red', model: 'VW Golf')
+Car.create(color: 'black', model: 'VW Jetta', car_share_member_id: 2)
+Car.create(color: 'white', model: 'Toyota Celica')
+
+CarShareMember.create(first_name: 'Jen', username: 'Jen', password_digest: BCrypt::Password.create("Jen"))
+CarShareMember.create(first_name: 'Ted', username: 'Ted', password_digest: BCrypt::Password.create("Ted"))
