d0fc9f6edf46fcb327e8ed505baa2b650fceda0a
***infinite recursion in variable git_username*** <***infinite recursion in variable git_email***>
2013-06-14 07:02:34 -0600

    asdf

---
+++ 314/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '28361aab92e443a907573455e0ca6be6b346c96c4f0f27ac160977ba1faa1bac2557da47538c8b85bcc9e557a6460fcee05f9e6446715b0278001ca6c87e044a'
