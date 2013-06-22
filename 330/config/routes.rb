db920296b2b6c9dc44992dde04d7f34fab581268
Daniel Stutzman <dtstutz@gmail.com>
2013-06-22 17:14:32 -0600

    Convert routes.rb from 1.8 to 1.9 hash style.

--- 330/config/routes.rb
+++ 330/config/routes.rb
@@ -1,7 +1,7 @@
 WebApp::Application.routes.draw do
   get "/" => redirect("/users")
 
-  resources :users, :only => ["index", "new", "create"]
-  post "/users/login"  => "users#login",  :as => "login"
-  post "/users/logout" => "users#logout", :as => "logout"
+  resources :users, only: ["index", "new", "create"]
+  post "/users/login"  => "users#login",  as: "login"
+  post "/users/logout" => "users#logout", as: "logout"
 end
--- 334/config/routes.rb
+++ 334/config/routes.rb
@@ -1,9 +1,9 @@
 WebApp::Application.routes.draw do
   get "/" => redirect("/users")
 
-  resources :users, :only => ["index", "new", "create"]
-  post "/users/login"  => "users#login",  :as => "login"
-  post "/users/logout" => "users#logout", :as => "logout"
+  resources :users, only: ["index", "new", "create"]
+  post "/users/login"  => "users#login",  as: "login"
+  post "/users/logout" => "users#logout", as: "logout"
 
   get "/verify_email/:user_id/:token" => "users#verify_email",
     as: "verify_email"
